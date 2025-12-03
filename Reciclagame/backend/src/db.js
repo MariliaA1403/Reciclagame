6709bcfabbf415916f3899ec3d7e434466402297
MariliaA1403 <marilia.albuquerquelima@gmail.com>
2025-12-03 19:26:34 +0000

    exclusão de pastas duplicadas

--- Reciclagame/backend/src/db.js
+++
@@ -1,8 +0,0 @@
-// db.js
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_DuWMbk9aO0Qy@ep-small-glade-adamxh0q-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-});
-
-module.exports = pool;
